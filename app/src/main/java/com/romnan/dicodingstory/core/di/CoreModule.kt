4d7184da06aff35a9181e4f757f9e3ae548b5376
Deddy Romnan <romnandeddy@gmail.com>
2022-04-17 22:58:30 +0700

    fix: remove logging interceptor

--- app/src/main/java/com/romnan/dicodingstory/core/di/CoreModule.kt
+++ app/src/main/java/com/romnan/dicodingstory/core/di/CoreModule.kt
@@ -16,8 +16,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -36,18 +34,9 @@ class CoreModule {
     @Provides
     @Singleton
     fun provideCoreRetrofit(): Retrofit {
-        // TODO: remove logging interceptor
-        val interceptor = HttpLoggingInterceptor()
-            .apply { level = HttpLoggingInterceptor.Level.BODY }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
-
         return Retrofit.Builder()
             .baseUrl("https://story-api.dicoding.dev/v1/")
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
             .build()
     }
 
