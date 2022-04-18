fd0ca1ae980852b6af25bea8f6d2e5f178fb5cf3
Deddy Romnan <romnandeddy@gmail.com>
2022-04-18 19:11:48 +0700

    refactor: rename GetStoriesDto to GetStoriesResponse

--- app/src/main/java/com/romnan/dicodingstory/core/layers/data/retrofit/CoreApi.kt
+++ app/src/main/java/com/romnan/dicodingstory/core/layers/data/retrofit/CoreApi.kt
@@ -1,6 +1,6 @@
 package com.romnan.dicodingstory.core.layers.data.retrofit
 
-import com.romnan.dicodingstory.core.layers.data.retrofit.model.GetAllStoriesDto
+import com.romnan.dicodingstory.core.layers.data.retrofit.model.GetStoriesResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -10,12 +10,12 @@ interface CoreApi {
     suspend fun getStories(
         @Header("Authorization") bearerToken: String,
         @Query("location") withLocation: Int = CoreApiParamValues.WITH_LOCATION_FALSE
-    ): GetAllStoriesDto
+    ): GetStoriesResponse
 
     @GET("stories")
     suspend fun getPagedStories(
         @Header("Authorization") bearerToken: String,
         @Query("page") page: Int,
         @Query("size") size: Int
-    ): GetAllStoriesDto
+    ): GetStoriesResponse
 }
